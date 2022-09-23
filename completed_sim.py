9350c7cbf9502e59a95291b8bfda598b29337242
zhaeus <87953431+zhaeus@users.noreply.github.com>
2022-09-24 08:02:24 +1000

    Trying new method

--- completed_sim.py
+++ completed_sim.py
@@ -14,42 +14,87 @@ dir_path = r'C:\Users\znoll\OneDrive\Documents\aaaPersonal\Personal-2022\Persona
 # Iterate directory
 def count_file():
     count = 0
+    file_list = []
     for path in os.listdir(dir_path):
         # check if current path is a file
         if os.path.isfile(os.path.join(dir_path, path)):
             count += 1
-    # print('File count:', count)
-    return count 
+            file_list += path
+    return count, file_list
 
 def count_folder():
     count = 0
+    folder_list = []
     for path in os.listdir(dir_path):
         # check if current path is a file
         if os.path.isdir(os.path.join(dir_path, path)):
             count += 1
+            folder_list += path
     # print('Folder count:', count) 
-    return count
+    return count, folder_list
 
 
 ### Emailing ###
 email_sender = 'scriptaethelred@gmail.com'
 # email_password = os.environ.get("EMAIL_PASSWORD")
-email_password = 'abwzytrirrysqisr'
-# email_password = 'Aethelred968AD'
+# email_password = 'abwzytrirrysqisr'
+email_password = 'Aethelred968AD'
 email_receiver = 'nollerzeke@gmail.com'
 
 subject = 'ANSYS Simulation Update'
 
-from email.message import EmailMessage
-import ssl
+# import ssl
 import smtplib
 
-context = ssl.create_default_context()
-def send_email(body):
+# Import smtplib for the actual sending function
+
+# Import the email modules we'll need
+from email.message import EmailMessage
+
+
+# Method 3
+def send_email3(body):
+    msg = EmailMessage()
+    msg.set_content(body)
+    
+    # me == the sender's email address
+    # you == the recipient's email address
+    msg['Subject'] = 'Simulation Update'
+    msg['From'] = email_sender
+    msg['To'] = email_receiver
+    
+    # Send the message via our own SMTP server.
+    s = smtplib.SMTP('localhost')
+    s.send_message(msg)
+    s.quit()
+
+
+# Method 2
+def send_email2(body):
+    context = ssl.create_default_context()
+
+    # Instantiating SMTP
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    
+    # Authentication 
+    s.login("scriptaethelred@gmail.com","abwzytrirrysqisr")
+    
+    # Sending 
+    s.sendmail(email_sender, email_receiver, body)
+ 
+    # Quitting SMTP
+    s.quit()
+    
+    
+# Method 1
+import ssl 
+def send_email1(body):
+    context = ssl.create_default_context()
     em = EmailMessage()
     em['From'] = email_sender
     em['To'] = email_receiver
-    em['Subject'] = subject
+    em['Subject'] = 'Simulation Update'
     em.set_content = (body)
     with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
         smtp.login(email_sender,email_password)
@@ -58,19 +103,22 @@ def send_email(body):
 
 
 ### Loop ###
-init_foldercount = count_folder()
-init_filecount = count_file()
-while True:
-    # dir_path = r'C:\Users\znoll\OneDrive\Documents\aaaPersonal\Personal-2022\Personal-Projects-2022\Squat-Rack'
-    
-    if count_folder() != init_foldercount:
-        print("New folder has been added")
-        body = """ New folder has been added"""
-        # break
-    if count_file() != init_filecount:
-        print("New file has been added!")
-        break
-    time.sleep(5)
+init_foldercount = count_folder()[0]
+init_filecount = count_file()[0]
+
+# while True:
+#     if count_folder()[0] != init_foldercount:
+#         print("Simulation update: new folder added!")
+#         print(f"Current list of folders is: {count_folder()[1]}")
+#         send_email(f"Simulation update: new folder added! \
+#                    Current list of folders is {count_folder()[1]}")
+#         # body = """ New folder has been added"""
+#     if count_file()[0] != init_filecount:
+#         print("Simulation update: new file added!")
+#         print(f"Current list of files is: {count_file()[1]}")
+#         send_email(f"Simulation update: new file added! \
+#                    Current list of files is {count_file()[1]}")
+#     time.sleep(10)
 
 
 
